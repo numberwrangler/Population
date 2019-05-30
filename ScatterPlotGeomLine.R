800faa15e1c4f3f798144225189818fabf866187
Brenda Lawall <rustler36.bk@gmail.com>
2019-05-30 13:34:11 -0600

    Create ScatterPlotGeomLine.R
    Example of GeomLine

---
+++ ScatterPlotGeomLine.R
@@ -0,0 +1,13 @@
+# scatter plot
+library(ggplot2)
+a <- ggplot(dataset, aes( x=year), color = variable)
+a + geom_point(aes(y = Age4, col= "Age4"))+
+  geom_line(aes(y=Age4, col="Age4"))+
+  geom_point(aes(y=Age9, col="Age9"))+
+  geom_line(aes(y=Age9, col="Age9"))+
+  geom_point(aes(y=Age14, col="Age14"))+
+  geom_line(aes(y=Age14, col="Age14"))+
+  geom_point(aes(y=Age19, col="Age19"))+
+  geom_line(aes(y=Age19, col="Age19"))
+
+str(dataset)
